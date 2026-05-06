525e2657cacf8b08156f6c47cf3757d7a39824af
entrinax22 <mark.entrina12@gmail.com>
2026-05-06 19:42:28 +0800

    feat: seed admin account on startup and make seeder idempotent

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,40 +12,48 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // Admin account
-        User::create([
-            'name' => 'Admin User',
-            'email' => 'admin@lms.test',
-            'password' => Hash::make('password'),
-            'role' => 'admin',
-            'email_verified_at' => now(),
-        ]);
+        User::updateOrCreate(
+            ['email' => 'admin@lms.test'],
+            [
+                'name' => 'Admin User',
+                'password' => Hash::make('password'),
+                'role' => 'admin',
+                'email_verified_at' => now(),
+            ]
+        );
 
         // Faculty account
-        User::create([
-            'name' => 'Faculty User',
-            'email' => 'faculty@lms.test',
-            'password' => Hash::make('password'),
-            'role' => 'faculty',
-            'email_verified_at' => now(),
-        ]);
+        User::updateOrCreate(
+            ['email' => 'faculty@lms.test'],
+            [
+                'name' => 'Faculty User',
+                'password' => Hash::make('password'),
+                'role' => 'faculty',
+                'email_verified_at' => now(),
+            ]
+        );
 
         // Student user + linked student profile
-        $studentUser = User::create([
-            'name' => 'Juan Dela Cruz',
-            'email' => 'student@lms.test',
-            'password' => Hash::make('password'),
-            'role' => 'student',
-            'email_verified_at' => now(),
-        ]);
+        $studentUser = User::updateOrCreate(
+            ['email' => 'student@lms.test'],
+            [
+                'name' => 'Juan Dela Cruz',
+                'password' => Hash::make('password'),
+                'role' => 'student',
+                'email_verified_at' => now(),
+            ]
+        );
 
-        Student::create([
-            'user_id' => $studentUser->id,
-            'student_no' => '2024-00001',
-            'first_name' => 'Juan',
-            'last_name' => 'Dela Cruz',
-            'email' => 'student@lms.test',
-            'course' => 'BSIT',
-            'year_level' => 2,
-        ]);
+        Student::updateOrCreate(
+            ['user_id' => $studentUser->id],
+            [
+                'student_no' => '2024-00001',
+                'first_name' => 'Juan',
+                'last_name' => 'Dela Cruz',
+                'email' => 'student@lms.test',
+                'course' => 'BSIT',
+                'year_level' => 2,
+            ]
+        );
     }
 }
--- docker/start.sh
+++ docker/start.sh
@@ -4,6 +4,9 @@ set -e
 echo "==> Running migrations..."
 php artisan migrate --force
 
+echo "==> Seeding database..."
+php artisan db:seed --force
+
 echo "==> Caching config, routes, views..."
 php artisan optimize
 
